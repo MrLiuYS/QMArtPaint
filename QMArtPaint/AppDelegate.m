74b2f695e5bc1a90273356f5f81014a2d18acc1f
Mr.Liu <3050700400@qq.com>
2016-06-10 23:29:21 +0800

    ....

--- QMArtPaint/AppDelegate.m
+++ QMArtPaint/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 
+#import <BmobSDK/Bmob.h>
+
 @interface AppDelegate ()
 
 @end
@@ -17,6 +19,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [Bmob registerWithAppKey:@""];
+    
     return YES;
 }
 
